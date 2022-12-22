99ce9f5d9a7b82db01ba33bda67bfd454b029c55
iwuji1 <obinnaops@gmail.com>
2022-12-22 09:13:30 +0000

    Updates

--- src/routes/portfolio/+page.svelte
+++
@@ -1,6 +0,0 @@
-<svelte:head>
-<title> Portfolio Page</title>
-</svelte:head>
-
-<h1> A Portfolio of My work </h1>
-<p> A portfolio of my work </p>
--- src/routes/uses/+page.md
+++
@@ -1,7 +0,0 @@
-# Uses
-
-**Here's some stuff I use**
-
-- SvelteKit
-- Atom
-- Emojis 😎
